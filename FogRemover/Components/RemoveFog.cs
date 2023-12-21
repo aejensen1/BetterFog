27c18ff5d551dbacaeab02a926dc1391b45585e7
vibeside <77847352+vibeside@users.noreply.github.com>
2023-12-21 10:19:05 -0500

    Add files via upload

---
+++ FogRemover/Components/RemoveFog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace FogRemover.Components
+{
+    internal class RemoveFog : MonoBehaviour
+    {
+        public Volume vol;
+        public Fog fog;
+        public void Awake()
+        {
+            FogRemover.mls.LogInfo("Removing fog...");
+        }
+        public void Update()
+        {
+            
+            if (vol == null)
+            {
+                foreach (Volume v in FindObjectsOfType<Volume>().ToList())
+                {
+                    if (v.gameObject.name == "VolumeMain" && !FogRemover.disableAll.Value)
+                    {
+                        vol = v;
+                    }
+                    if (FogRemover.disableAll.Value && v != null)
+                    {
+                        v.sharedProfile.TryGet(out fog);
+                        if (fog != null)
+                        {
+                            fog.active = false;
+                            fog.enabled.value = false;
+                            fog.enabled.overrideState = false;
+                        }
+                        vol = v;
+                    }
+                }
+                if (fog == null && vol != null && !FogRemover.disableAll.Value)
+                {
+                    vol.sharedProfile.TryGet(out fog);
+                }
+            }
+            if (fog != null && !FogRemover.disableAll.Value)
+            {
+                if (fog.enabled.value)
+                {
+                    fog.enabled.value = false;
+                    fog.enabled.overrideState = false;
+                    fog.active = false;
+                    FogRemover.mls.LogInfo("Fog removed");
+                }
+            }
+        }
+    }
+}
---
+++ FogRemover/FogRemover.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HarmonyLib;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+using UnityEngine.Rendering;
+using FogRemover.Components;
+using BepInEx.Configuration;
+
+namespace FogRemover
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class FogRemover : BaseUnityPlugin
+    {
+        public const string modGUID = "grug.lethalcompany.fogremover";
+        public const string modName = "Remove fog";
+        public const string modVersion = "0.1.0.0";
+
+        //logger.loginfo(""); to log
+        private readonly Harmony harmony = new Harmony(modGUID);
+        public static ManualLogSource mls;
+        private static FogRemover instance;
+        public static GameObject bruh;
+        public static ConfigEntry<bool> disableAll;
+        public void Awake()
+        {
+            disableAll = Config.Bind("General","All fog disable?", false, "This may cause major amounts of lag. use at your own risk");
+            mls = base.Logger;
+            if (instance == null)
+            {
+                instance = this;
+            }
+            if (bruh == null)
+            {
+                bruh = new GameObject("FogRemoverHolder");
+                DontDestroyOnLoad(bruh);
+                bruh.hideFlags = HideFlags.HideAndDontSave;
+                bruh.AddComponent<RemoveFog>();
+            }
+
+        }
+    }
+}
