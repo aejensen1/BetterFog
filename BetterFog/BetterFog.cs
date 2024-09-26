979ce08204faf6845aa01ce19a551ae7280efa03
aejensen1 <aejense@gmail.com>
2024-09-26 14:05:29 -0400

    Change version number

--- BetterFog/BetterFog.cs
+++ BetterFog/BetterFog.cs
@@ -23,7 +23,7 @@ namespace BetterFog
     {
         public const string modGUID = "ironthumb.BetterFog";
         public const string modName = "BetterFog";
-        public const string modVersion = "3.2.7";
+        public const string modVersion = "3.2.8";
 
         private readonly Harmony harmony = new Harmony(modGUID);
         public static ManualLogSource mls;
